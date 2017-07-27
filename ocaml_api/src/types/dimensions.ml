7ae2f89d2fa2ea809df350700c2ec14611f6a84e
Colin González <colin.gonzalez@openmailbox.org>
2017-07-27 17:18:39 +0200

    Fix type in the dimensions module name

--- ocaml_api/src/types/dimensions.ml
+++ ocaml_api/src/types/dimensions.ml
@@ -3,8 +3,8 @@ type t = {
   width  : int;
 } [@@deriving yojson]
 
-let dimension_to_json d =
+let dimensions_to_json d =
   Yojson.Safe.to_string (to_yojson d)
 
-let dimension_of_json j =
+let dimensions_of_json j =
   of_yojson (Yojson.Safe.from_string j)
